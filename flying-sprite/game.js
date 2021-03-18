2896ab6e711250dae3ba0125ab85c6960c1f515b
andres-thefirst <darkshadowskull@gmail.com>
2021-03-18 00:03:29 -0700

    Add cannon that moves pointing to the cursor/mouse

--- flying-sprite/game.js
+++ flying-sprite/game.js
@@ -3,64 +3,62 @@ var Game = {
   canvas: undefined,
   canvasContext: undefined,
   backgroundSprite: undefined,
-  balloonSprinte: undefined,
-  balloonSprinte1: undefined,
-  balloonSprinte2: undefined,
-  balloonSprinte3: undefined,
+  cannonBarrelSprite: undefined,
+  balloonSprite: undefined,
   balloonPosition: {x: 0, y: 50},
-  balloonPosition1: {x: 0, y: 80},
-  balloonPosition2: {x: 0, y: 120},
-  balloonPosition3: {x: 0, y: 150},
-  backgroundMusic: undefined
+  balloonOrigin: {x : 0, y: 0},
+  mousePosition: { x: 0, y: 0},
+  cannonPosition: { x: 72, y: 405},
+  cannonOrigin: { x: 34, y: 34},
+  cannonRotation: 0
 };
 
 Game.start = () =>  { 
   Game.canvas = document.getElementById("myCanvas"); 
   Game.canvasContext = Game.canvas.getContext("2d"); 
-  Game.balloonSprinte = new Image();
-  Game.balloonSprinte.src = "sprites/spr_balloon.png";
-  Game.balloonSprinte1 = new Image();
-  Game.balloonSprinte1.src = "sprites/spr_balloon.png";
-  Game.balloonSprinte2 = new Image();
-  Game.balloonSprinte2.src = "sprites/spr_balloon.png";
-  Game.balloonSprinte3 = new Image();
-  Game.balloonSprinte3.src = "sprites/spr_balloon.png";
+  Game.balloonSprite = new Image();
+  Game.balloonSprite.src = "sprites/spr_balloon.png";
   Game.backgroundSprite = new Image();
-  Game.backgroundSprite.src = "sprites/spr_background.jpg";
-  Game.backgroundMusic = new Audio();
-  Game.backgroundMusic.src = "snd_music.mp3";
-  Game.backgroundMusic.play();
-  Game.backgroundMusic.volume = 0.4;
+  Game.backgroundSprite.src = "sprites/spr_background.jpg"
+  Game.cannonBarrelSprite = new Image();
+  Game.cannonBarrelSprite.src = "sprites/spr_cannon_barrel.png";
   window.setTimeout(Game.mainLoop, 500);
 } ;
 
 document.addEventListener('DOMContentLoaded', Game.start); 
 
 Game.update =  () => {
-  Game.balloonPosition.x = new Date().getTime() % Game.canvas.width;
-  Game.balloonPosition1.x = new Date().getTime() * 2 % Game.canvas.width;
-  Game.balloonPosition2.x = new Date().getTime() / 2 % Game.canvas.width;
-  Game.balloonPosition3.x = new Date().getTime() / 3 % Game.canvas.width;
- };
+  const opposite =  Game.mousePosition.y - Game.cannonPosition.y;
+  const adjacent =  Game.mousePosition.x - Game.cannonPosition.x;
+  Game.cannonRotation = Math.atan2(opposite, adjacent);
+};
 
 Game.draw = () => { 
-  Game.drawImage(Game.backgroundSprite, {x: 0, y: 0});
-  Game.drawImage(Game.balloonSprinte, Game.balloonPosition);
-  Game.drawImage(Game.balloonSprinte1, Game.balloonPosition1);
-  Game.drawImage(Game.balloonSprinte2, Game.balloonPosition2);
-  Game.drawImage(Game.balloonSprinte3, Game.balloonPosition3);
+  Game.drawImage(Game.backgroundSprite, {x: 0, y: 0}, 0);
+  Game.balloonOrigin = {x : Game.balloonSprite.width / 2, y : Game.balloonSprite.height / 2};
+  Game.drawImage(Game.balloonSprite, Game.balloonPosition, 0, Game.balloonOrigin);
+  Game.drawImage(Game.cannonBarrelSprite, Game.cannonPosition, Game.cannonRotation, Game.cannonOrigin);
 } ;
 
 Game.clearCanvas = function () {
   Game.canvasContext.clearRect(0, 0, Game.canvas.width, Game.canvas.height);
 }
 
-Game.drawImage = function (sprite, postion) {
+Game.drawImage = function (sprite, postion, rotation, origin = {x: 0, y: 0}) {
   Game.canvasContext.save();
   Game.canvasContext.translate(postion.x, postion.y);
-  Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, 0, 0, sprite.width, sprite.height);
+  Game.canvasContext.rotate(rotation);
+  Game.canvasContext.drawImage(sprite, 0, 0, sprite.width, sprite.height, -origin.x, -origin.y, sprite.width, sprite.height);
   Game.canvasContext.restore();
 };
+
+function handleMouseMove(evt) {
+  Game.balloonPosition = {x : evt.pageX, y: evt.pageY};
+  Game.mousePosition = {x : evt.pageX, y: evt.pageY};
+}
+
+document.onmousemove = handleMouseMove;
+
 
 Game.mainLoop = () => { 
   Game.clearCanvas();
